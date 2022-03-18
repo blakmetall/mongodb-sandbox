aa41fa5494dc31e2a296926066a2942d5d583982
Jose Faro <blakmetall@gmail.com>
2022-03-17 18:25:09 -0600

    init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+
+const db = config.get("mongoURI");
+
+mongoose
+  .connect(db, {})
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+const Animal = require("./models/Animal");
+
+const newAnimal = new Animal({
+  name: "Red Panda",
+  isEndangered: true,
+});
+
+newAnimal
+  .save()
+  .then((item) => console.log(item))
+  .catch((err) => console.log(err));

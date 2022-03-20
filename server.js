0d988ea2d3641982006cd9b07207e5b273d000b6
Jose Faro <blakmetall@gmail.com>
2022-03-19 20:47:03 -0600

    routes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const config = require("config");
 
 const app = express();
+app.use(express.json());
 
 // gitignored configuration
 const db = config.get("mongoURI");
@@ -14,12 +16,35 @@ mongoose
 
 const Animal = require("./models/Animal");
 
-const newAnimal = new Animal({
-  name: "Red Panda",
-  isEndangered: true,
+// find all entries
+app.get("/", (req, res) => {
+  Animal.find()
+    .sort({ date: -1 })
+    .then((items) => console.log(res.json(items)));
 });
 
-newAnimal
-  .save()
-  .then((item) => console.log(item))
-  .catch((err) => console.log(err));
+// add a new entry
+app.post("/", (req, res) => {
+  const newAnimal = new Animal({
+    name: req.body.name,
+    isEndangered: req.body.isEndangered || false,
+  });
+  newAnimal.save().then((item) => res.json(item));
+});
+
+// delete an entry
+app.delete("/:id", (req, res) => {
+  Animal.findOneAndDelete({ _id: req.params.id })
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+// update an entry
+app.put("/:id", (req, res) => {
+  Animal.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(() => res.json({ success: true }))
+    .catch((err) => res.status(404).json({ success: false }));
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`Server started on port: ${port}`));
